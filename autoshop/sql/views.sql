0d0ecd953fc74bcc4c430c0b8aca9e08fc3cb809
Albert Luganga <ahosea15@gmail.com>
2019-08-23 17:09:43 +0300

    revise sql functions get_sales and get_expenses

--- autoshop/sql/views.sql
+++ autoshop/sql/views.sql
@@ -4,15 +4,16 @@ SELECT 'vendor' as group, uuid, name FROM vendor UNION
 SELECT 'entity' as group, uuid, name FROM entity UNION
 SELECT 'payment type' as group, uuid, name FROM payment_type;
 
-CREATE FUNCTION get_sales(start_date date, end_date date) RETURNS NUMERIC AS $$
-    SELECT sum(amount) FROM item_log
-    WHERE category='sale'
-    and date_created>=start_date
-    and date_created<=end_date;
+CREATE OR REPLACE FUNCTION get_sales(date DATE) RETURNS NUMERIC AS $$
+    SELECT sum(cast(amount as NUMERIC)) FROM item_log
+    WHERE TO_CHAR(date_created :: DATE, 'yyyy-mm-dd')=cast(date as VARCHAR)
 $$ LANGUAGE SQL;
 
-CREATE FUNCTION get_expenses(start_date date, end_date date) RETURNS NUMERIC AS $$
+
+
+CREATE OR REPLACE FUNCTION get_expenses(date DATE) RETURNS NUMERIC AS $$
     SELECT sum(cast(amount as NUMERIC)) FROM expense
-    WHERE date_created>=start_date
-    and date_created<=end_date;
+    WHERE TO_CHAR(date_created :: DATE, 'yyyy-mm-dd')=cast(date as VARCHAR)
 $$ LANGUAGE SQL;
+
+
