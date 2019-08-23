edd758e4b5c8862873a857dda79641ee0f92ab55
Albert Luganga <ahosea15@gmail.com>
2019-08-23 15:01:06 +0300

    new functions postgresql

--- autoshop/sql/views.sql
+++ autoshop/sql/views.sql
@@ -4,3 +4,15 @@ SELECT 'vendor' as group, uuid, name FROM vendor UNION
 SELECT 'entity' as group, uuid, name FROM entity UNION
 SELECT 'payment type' as group, uuid, name FROM payment_type;
 
+CREATE FUNCTION get_sales(start_date date, end_date date) RETURNS NUMERIC AS $$
+    SELECT sum(amount) FROM item_log
+    WHERE category='sale'
+    and date_created>=start_date
+    and date_created<=end_date;
+$$ LANGUAGE SQL;
+
+CREATE FUNCTION get_expenses(start_date date, end_date date) RETURNS NUMERIC AS $$
+    SELECT sum(cast(amount as NUMERIC)) FROM expense
+    WHERE date_created>=start_date
+    and date_created<=end_date;
+$$ LANGUAGE SQL;
