8b673c7fd765f241b58046be29cec5490f01086c
Albert Luganga <ahosea15@gmail.com>
2019-09-16 13:10:00 +0300

    add commission account to account holders view

--- autoshop/sql/views.sql
+++ autoshop/sql/views.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE VIEW account_holders AS
 SELECT 'customer' as group, uuid, name as name FROM customer UNION
 SELECT 'vendor' as group, uuid, name FROM vendor UNION
 SELECT 'entity' as group, uuid, name FROM entity UNION
+SELECT 'commission account' as group, uuid, name FROM commission_account UNION
 SELECT 'payment type' as group, uuid, name FROM payment_type;
 
 CREATE OR REPLACE FUNCTION get_sales(date DATE) RETURNS NUMERIC AS $$
@@ -14,14 +15,14 @@ CREATE OR REPLACE FUNCTION get_expenses(date DATE) RETURNS NUMERIC AS $$
     WHERE TO_CHAR(date_created :: DATE, 'yyyy-mm-dd')=cast(date as VARCHAR)
 $$ LANGUAGE SQL;
 
-CREATE FUNCTION get_total_sales(start_date date, end_date date) RETURNS NUMERIC AS $$
+CREATE OR REPLACE FUNCTION get_total_sales(start_date date, end_date date) RETURNS NUMERIC AS $$
     SELECT sum(amount) FROM item_log
     WHERE category='sale'
     and date_created>=start_date
     and date_created<=end_date;
 $$ LANGUAGE SQL;
 
-CREATE FUNCTION get_total_expenses(start_date date, end_date date) RETURNS NUMERIC AS $$
+CREATE OR REPLACE FUNCTION get_total_expenses(start_date date, end_date date) RETURNS NUMERIC AS $$
     SELECT sum(cast(amount as NUMERIC)) FROM expense
     WHERE date_created>=start_date
     and date_created<=end_date;
